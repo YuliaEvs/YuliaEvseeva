3b6fb79c48cba029e48ab6708ba899e5f9b4d7b2
Yulia Evseeva <evseevayk@gmail.com>
2024-03-18 23:24:04 -0400

    deleted cards

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -5,36 +5,28 @@ export default function Projects() {
         <div className='projects-container'>
         <div className='projects-wrapper'>
             <div className="card" id='p1'>
-                <div className="face flip-right">Project 1</div>
-                <div className="back flip-left">Description</div>
+
             </div>
             <div className="card" id='p2'>
-                <div className="face flip-down">Project 2</div>
-                <div className="back flip-up">Description</div>
+
             </div>
             <div className="card" id='p3'>
-                <div className="face flip-left">Project 3</div>
-                <div className="back flip-right ">Description</div>
+
             </div>
             <div className="card" id='p4'>
-                <div className="face flip-up">Project 4</div>
-                <div className="back flip-down">Description</div>
+
             </div>
             <div className="card" id='p5'>
-                <div className="face flip-down">Project 5</div>
-                <div className="back flip-up">Description</div>
+
             </div>
             <div className="card" id='p6'>
-                <div className="face flip-left">Project 6</div>
-                <div className="back flip-right">Description</div>
+
             </div>
             <div className="card" id='p7'>
-                <div className="face flip-up">Project 7</div>
-                <div className="back flip-down">Description</div>
+
             </div>
             <div className="card" id='p8'>
-                <div className="face flip-right">Project 8</div>
-                <div className="back flip-left">Description</div>
+
             </div>
 
         </div>
