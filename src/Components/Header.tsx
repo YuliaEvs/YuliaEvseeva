f6bfd2c701f3cc33627728851d482079b8f41d70
Yulia Evseeva <evseevayk@gmail.com>
2024-03-23 21:50:18 -0400

    globe

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,24 +1,14 @@
 import './header.css';
 import { useState } from 'react';
 
-
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    // const [isActive, setIsActive] = useState(false);
 
     function handleMenu() {
         setIsMenuOpen((prevState) => !prevState);
-        // setIsActive(!isActive);
 
         document.body.style.overflow = isMenuOpen ? "unset" : "hidden";
     }
-
-    // const lineStyles = (line: number) => ({
-    //     transform: isMenuOpen
-    //       ? `translateY(${line === 1 ? '' : '-'}0.75rem) rotate(${line === 1 ? '' : '-'}45deg)`
-    //       : 'none',
-    //     transition: 'transform 0.3s ease-in-out',
-    // });
 
     return (
         <header>
@@ -26,37 +16,38 @@ export default function Navbar() {
                 <div className='header-logo-wrapper'>
                     <a href='#contact'></a>
                 </div>
-                <div className='submenu-wrap'>
-                    <p className='submenu'>You-lee-ya</p>
-                </div>
             </div>
             <div className='navbar'>
-                    <button className="changeMode">
+                <div className="changeMode">
+                    <button className='lamp'>
                         <span className="material-symbols-rounded">light</span>
                     </button>
-
+                </div>
                     <div className='nav-burger' onClick={handleMenu}>
                        
-                            <div className={`nav-burger-wrap ${isMenuOpen ? 'active' : ""}`}>
-                                
-                                <div className='burger-line-1'></div>
-                                <div className='burger-line-2'></div>
-                            </div>
-                  
+                        <div className={`nav-burger-wrap ${isMenuOpen ? 'active' : ""}`}>
+                            <div className='burger-line-1'></div>
+                            <div className='burger-line-2'></div>
+                        </div>
 
-                            <div className='menu-content' style={{display: isMenuOpen ? 'block' : 'none'}}>
-                                {   
-                                    <div className='menu'>
-                                        <a href='#'>INFO</a>
-                                        <p className='submenu'>about me</p>
-                                        <a href='#'>WORK</a>
-                                        <a href='#'>CONTACT</a>
-                                        <p className='submenu'>for collaboration</p>
-                                    </div>    
-                                }
-                            </div>
+                    <div className='menu-content' style={{display: isMenuOpen ? 'block' : 'none'}}>
+                        {   
+                            <div className='menu'>
+                                <a href='#'>INFO</a>
+                                <p className='resume'>Download Resume</p>
+                                    <div className="link-wrap">
+                                        <span className="material-symbols-outlined">
+                                            data_loss_prevention
+                                        </span>
+                                    </div>
+                                    <a href='#'>WORK</a>
+                                    <a href='#'>CONTACT</a>
+                                    <p className='submenu'>for collaboration</p>
+                            </div>    
+                        }
+                    </div>
 
-                    </div> 
+                </div> 
 
             </div>
         </header>
--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,16 +1,68 @@
 import './hero.css';
-
+import heroImage from '../assets/hero-img-1.jpeg'
 
 export default function Hero() {
     
     return (
         <section>
             <div className='hero-container'>
+                <div className="globe-wrap">
+                    <div className="globe"></div>
+                    <div className="location-wrap">
+                        <span>Virginia</span>
+                        <span>USA</span>
+                    </div>
+                </div>
                 <div className='hero-wrapper'>
+                    <div id="hero-img-1">
+                        <img alt="Profile Image"
+                            className="hero-img rounded shadow-lg"
+                            height="auto"
+                            width="700px"
+                            src={heroImage}
+                        />
+                    </div>
+                    <div className="about">
+                        <div className='greeting'>
+                           
+                            Hello and Welcome, Friend! I'm Yulia, a Code Whisperer 🧙‍♂️ or a Junior Software Engineer 👨‍💻🚀
+                        <div>
+                        </div>
+                        <div>
+                        <div className="about-content-container" onclick="toggleSubmenu('submenu1')">
+                            <div className='dropdown-toggle'/>My Toolbox 🛠️</div>
+                                <div className='dropdown-toggleSubmenu'/>
+                        </div>
+                        <div>
+                        <div className="about-content-container" onclick="toggleSubmenu('submenu1')">
+                            <div className='dropdown-toggle'/>Fun Facts 🎉</div>
+                                <div className='dropdown-toggleSubmenu'>
+                                - I believe in the power of `ctrl + z`.
+                                - My favorite debugging tool is hope.
+                                - I'm on a first-name basis with the `404 Error`.
+                                - I craft code that sometimes even surprises me.
+                                - I have intense debates with my computer - and occasionally, I win.
+                            </div>
+                        </div>
+                        <div>
+                        <div className="about-content-container" onclick="toggleSubmenu('submenu1')">
+                            <div className='dropdown-toggle'/>The Dream 🌟</div>
+                            <h3 className='about-submenu'>
+                                One day, I aspire to use my powers for good, automating everything to the point where my coffee machine will start brewing just by thinking about it. Until then, I'll keep translating human ideas into computer actions, one pixel at a time.
+                                `So, if you need someone to turn your `if` into reality, or your `else` into success, you know where to find me. Let's make magic happen – with code!</h3>
+                        </div>
+                        <div>
+                        <div className="about-content-container" onclick="toggleSubmenu('submenu1')">
+                           <div className='dropdown-toggle'/>Beyond the Code 🚀</div>
+                           <h3 className='about-submenu'>
+                            When I'm not on a quest to squash bugs or decipher cryptic error messages, you might catch me ... 🤔 umm... no, it's all that I am currently doing 😳</h3>
+                        </div>
+                            
+                        </div>
+                    </div>
                     
                 </div>
-             </div>
-            
+            </div> 
         </section>
     )
 }
