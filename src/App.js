22ccd14ff68258b82a5319fa215b558a1b7d385c
Yulia Evseeva <evseevayk@gmail.com>
2024-04-26 13:01:37 -0400

    correct info

--- src/App.js
+++ src/App.js
@@ -138,8 +138,8 @@ ScrollReveal({
  });
 
  ScrollReveal().reveal(".home .info h2, section-title-01, section-title-02", { delay: 500, origin: "left" });
- ScrollReveal().reveal(".home .info h3, .home .info p, .about-info .btn", { delay: 600, origin: "right" });
- ScrollReveal().reveal(".home .info .btn", { delay: 700, origin: "bottom" });
+ ScrollReveal().reveal(".home .info h3, .home .info p", { delay: 600, origin: "right" });
+ ScrollReveal().reveal(".home .info .btn, .about-info .btn", { delay: 700, origin: "bottom" });
  ScrollReveal().reveal(".madia-icons i, .contact-left li", { delay: 500, origin: "left", interval: 200 });
  ScrollReveal().reveal(".home-img, about-img", { delay: 500, origin: "bottom" });
  ScrollReveal().reveal(".about .description, .copy-right", { delay: 600, origin: "right" });
